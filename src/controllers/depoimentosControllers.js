2f60135ed6e1d131eb42fa6944c1d8afb3b6a52a
Kauê Barreto Benicio <kauebarretobenicio@gmail.com>
2025-11-27 04:58:26 -0300

    Rota para retornar 3 depoimentos aleatórios
    Criação da rota para retornar3 depoimentos aleatórios.

--- src/controllers/depoimentosControllers.js
+++ src/controllers/depoimentosControllers.js
@@ -6,6 +6,31 @@ async function todosDepoimentos(req, res) {
     res.status(200).json(depoimentos);
 }
 
+//Retorna 3 depoimentos randomicos
+async function tresDepoimentosAleatorios(req, res) {
+    try {
+        const filterDepoiments = [];
+        const randomNumberList = [];
+
+        while (filterDepoiments.length < 3) {
+            const randomNumber = Math.floor(Math.random() * depoimentos.length);
+
+            if (randomNumberList.includes(randomNumber)) {
+                continue; 
+            }
+
+            randomNumberList.push(randomNumber);
+            filterDepoiments.push(depoimentos[randomNumber]);
+        }
+
+        return res.status(200).json(filterDepoiments);
+
+    } catch (err) {
+        return res.status(400).json({ error: err.message });
+    }
+}
+
 module.exports = {
-    todosDepoimentos
+    todosDepoimentos,
+    tresDepoimentosAleatorios
 }
--- src/routes/depoimentos.js
+++ src/routes/depoimentos.js
@@ -6,4 +6,7 @@ const depoimentosControllers = require("../controllers/depoimentosControllers");
 //Rota para retornar todos os depoimentos
 router.get("/depoimentos", depoimentosControllers.todosDepoimentos);
 
+//Rota para retornar 3 depoimentos randomicos
+router.get("/depoimentos-home", depoimentosControllers.tresDepoimentosAleatorios);
+
 module.exports = router;
